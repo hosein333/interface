7827f197926e25a8c89c6e7bf3cdf159c05c8890
mohammadjavad <hossinbabayy1382@gmail.com>
2024-02-13 14:47:34 +0330

    interface types

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 //Shape shape interface
@@ -27,14 +28,47 @@ type Rectangle struct {
 func (r Rectangle) Area() int {
     return r.Lenght * r.Width
 }
+func printLog(pChannel chan interface{}) {
+    for {
+        v, ok := <-pChannel
 
+        if !ok {
+            fmt.Println("pChannel Closed")
+            return
+        }
+
+        fmt.Println(v)
+    }
+}
 func main() {
     s := Square{Length: 10}
     r := Rectangle{Lenght: 10, Width: 20}
+
+        LogChannel := make(chan interface{}, 10)
+
+            go printLog(LogChannel)
+
+            time.Sleep(1 * time.Second)
+
+
+            LogChannel <- "test"
+            LogChannel <- "1234"
+            LogChannel <- "!@#4"
+            LogChannel <- "t1!"
+
     
     shapeList := []Shape{s, r}
 
     for _, value := range shapeList {
-        fmt.Printf("type : %T, Area : %d\n", value, value.Area())
+        fmt.Printf("\ntype : %T, Area : %d\n", value, value.Area())
+        square, ok := value.(Square)
+        fmt.Printf("type: %T , OK : %v\n", square, ok)
     }
+    res := CalculateArea(s)
+    fmt.Println(res)
+}
+
+// CalculateArea godoc
+func CalculateArea(s Shape) int {
+    return s.Area()
 }
